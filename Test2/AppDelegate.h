d920f1d9d730071ea1fb42ed2687995bab4768e8
Mayuran <Mayuran@Sajis-iMac.local>
2013-09-18 13:16:26 +0800

    Initial Commit

---
+++ Test2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test2
+//
+//  Created by Mayuran on 18/9/13.
+//  Copyright (c) 2013 Mayuran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test2
+//
+//  Created by Mayuran on 18/9/13.
+//  Copyright (c) 2013 Mayuran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
